60b3e87338d4bee58bbabd9e44f934a1238318cf
Joshua Kent <kentjo@pascack.org>
2019-02-26 09:16:52 -0500

    haven't committed in a while, so idk

--- deploy.py
+++ deploy.py
@@ -22,13 +22,13 @@ def graphicDesign():
 def contact():
     return render_template("contact.html")
 
-@app.route("/gitHub")
-def gitHub():
-    return render_template("gitHub.html")
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
 
-@app.route("/javaScriptProjects")
-def javaScriptProjects():
-    return render_template("javaScriptProjects.html")
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
